ddc4558b11511daa702bdb145925d4c8bad2c995
Examliquems <ali33uygun@hotmail.com>
2024-10-01 18:12:58 +0300

    Low Power Mode Exercise
    Main goal : TIM6 triggers update interrupt for every 10 ms and in the ISR of TIM6 send some data over UART. Measure the current consumption before and after adding EnableSleeopOnExit. It reduces consumption appr. 4.5 mA to 3mA

---
+++ sleep_on_exit/Core/Inc/main_app.h
@@ -0,0 +1,20 @@
+/*
+ * main_app.h
+ *
+ *  Created on: Sep 27, 2024
+ *      Author: ali33
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "stm32f4xx_hal.h"
+
+#define SYS_CLOCK_FREQ_50_MHZ   50
+#define SYS_CLOCK_FREQ_84_MHZ   84
+#define SYS_CLOCK_FREQ_120_MHZ  120
+
+#define TRUE  1
+#define FALSE 0
+
+#endif /* MAIN_H_ */
---
+++ sleep_on_exit/Core/Src/data.c
@@ -0,0 +1,13 @@
+/*
+ * data.c
+ *
+ *  Created on: Oct 1, 2024
+ *      Author: ali33
+ */
+
+
+
+#include "main_app.h"
+
+//some randomly generated text
+char some_data[] = "We are testing SLEEPONEXIT feature\r\n";
